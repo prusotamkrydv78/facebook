141e81f4e68d578e10ad6956d8733e22326dec6e
prusotamkrydv78 <prusotamkumaryadav78@gmail.com>
2025-02-17 18:29:28 +0545

    Add Lenis for smooth scrolling and implement new navbar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,46 @@
-import { Component, AfterViewInit } from '@angular/core';
-import gsap from 'gsap'; 
-import { LocomotiveScrollService } from './shared/services/locomotive.service';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, OnDestroy, ElementRef, ViewChild } from '@angular/core';
+import Lenis from '@studio-freight/lenis';
+import gsap from 'gsap';
 
 @Component({
   standalone: true,
   selector: 'app-root',
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewInit {
-  constructor(private scrollService:LocomotiveScrollService ) {}
+export class AppComponent {
+  title = 'facebook';
+  
+  constructor(
+
+  ) { }
+  private lenis!: Lenis
+
+  ngOnInit() {
+    this.lenis = new Lenis({
+      lerp: 0.09,  // smoothness of the scroll
+      smoothWheel: true,  // enables smooth scrolling with the wheel
+      // infinite:true
+    });
+
+    // Request animation frame for Lenis
+    const raf = () => {
+      this.lenis.raf(performance.now());
+      requestAnimationFrame(raf);
+    };
+    requestAnimationFrame(raf); 
+  }
+
+  
+
+  ngOnDestroy() {
+    // Clean up when the component is destroyed
+    this.lenis.destroy();
+  }
 
   ngAfterViewInit() {
-    this.scrollService.initScroll();
 
     gsap.from('.fade-in', {
       opacity: 0,
